fc44a89f62737bdcee28c9712b9c2531840fb310
sanjuthomas <sanju@sanju.org>
2013-07-05 22:34:39 +0530

    file lock

---
+++ ourownjava/src/com/ourownjava/corejava/file/FileLockExample.java
@@ -0,0 +1,40 @@
+package com.ourownjava.corejava.file;
+
+import java.io.*;
+import java.nio.channels.*;
+
+/**
+ * @date 1st May, 2011
+ * @author ourownjava.com
+ *
+ * To demonstrate the file lock API in java.nio
+ * Example program for java file lock
+ *
+ */
+
+public class FileLockExample {
+
+	private FileLock lock(RandomAccessFile file) throws IOException{
+		FileLock fileLock = file.getChannel().tryLock();
+		//if the JVM is not able to acquire a lock then a null
+		//is returned, it could be because the lock is already acquired
+		//by another thread or process.
+		return fileLock;
+	}
+
+	public static void main(String args[]) throws IOException, InterruptedException{
+		RandomAccessFile file = new RandomAccessFile(new File("/mnt/java/sample.file"), "rw");
+		FileLockExample fthis = new FileLockExample();
+		FileLock lock = fthis.lock(file);
+		System.out.println("Got the lock? "+(null != lock));
+		if(null == lock){
+			return;
+		}
+		while(true){
+			if(null != lock){
+				System.out.println("Is a valid lock? "+lock.isValid());
+			}
+			Thread.sleep(3000);
+		}
+	}
+}

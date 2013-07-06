986d4a4356e800e3994ca8d5ac28aa7a83d00e13
sanjuthomas <sanju@sanju.org>
2013-07-06 10:28:39 +0530

    thread

--- ourownjava/src/com/ourownjava/corejava/string/StringInternExample.java
+++ ourownjava/src/com/ourownjava/corejava/string/StringInternExample.java
@@ -9,12 +9,12 @@ package com.ourownjava.corejava.string;
  */
 public class StringInternExample {
 
-	public static void main(String args[]) {
+	public static void main(final String args[]) {
 
-		String str = "India";
-		String str1 = "India";
-		String str2 = new String("India");
-		String str3 = new String("India").intern();
+		final String str = "India";
+		final String str1 = "India";
+		final String str2 = new String("India");
+		final String str3 = new String("India").intern();
 
 		System.out.println("str == str1 : "+(str == str1));
 		System.out.println("str == str2 : "+(str == str2));
